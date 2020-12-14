5ed00b0d57b4e3e253e8220610e7e75defd6d148
DBKelly3192 <DavidBenjaminKelly@gmai.com>
2020-12-14 15:19:00 -0500

    modified mail route in 'app.py'

--- app.py
+++ app.py
@@ -51,7 +51,6 @@ mail = Mail(app)
 @app.route('/email/<id>')
 def index(id):
     soss = [model_to_dict(sos) for sos in current_user.soss]
-    print(soss[int(id)-1])
     msg = Message(
         f"{ current_user.first } { current_user.last } wanted you let you know they're hitting the trail!",
         recipients = [f"{ current_user.emergencyEmail }"]
